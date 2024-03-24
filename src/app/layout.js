4ea30186af4a594beceab12489b36d998f424880
Ofek Abramovitch <ofek301156@gmail.com>
2024-03-25 00:18:04 +0200

    added styling to header and greeeting

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google"
 import "./assets/styles/main.scss"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -10,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" type="image/png" sizes="50x50" href="/img/favicon.ico" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
