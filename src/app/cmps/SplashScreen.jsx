0229c8ad57e989fea63162719a601cfb54ec6887
Ofek Abramovitch <ofek301156@gmail.com>
2024-03-24 23:37:39 +0200

    some global styling

--- src/app/cmps/SplashScreen.jsx
+++ src/app/cmps/SplashScreen.jsx
@@ -9,9 +9,9 @@ export default function SplashScreen() {
                 <DisplayLottie animationData={splashScreen.animation} />
             </div>
             <div className="splash-title-container">
-                <span className="grey-color"> &lt</span>
+                <span className="grey-color"> &lt;</span>
                 <span className="splash-title">{greeting.username}</span>
-                <span className="grey-color">/&gt</span>
+                <span className="grey-color">/&gt;</span>
             </div>
         </div>
     )
