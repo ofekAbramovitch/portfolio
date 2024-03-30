dcc53e500664bc38745c6e20340af7d43115d337
Ofek Abramovitch <ofek301156@gmail.com>
2024-03-30 23:16:18 +0300

    changes to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" prefix="og: http://ogp.me/ns#">
       <Head>
-        <link rel="icon" type="image/png" sizes="50x50" href="public/favicon.ico" />
+        <title>{metadata.title}</title>
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content="Ofek Abramovitch Portfolio" />
+        <meta property="og:image" content="https://res.cloudinary.com/ofekabramovitch/image/upload/v1711829505/dlxb8x59l7kytj5jg5a2.png" />
+        <meta property="og:url" content="https://ofek-abramovitch-portfolio.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <link rel="icon" type="image/png" sizes="50x50" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome@5.15.4/css/all.min.css" />
       </Head>
       <body className={inter.className}>{children}</body>
